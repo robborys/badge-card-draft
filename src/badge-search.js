8f6fa872de5458fe50befa25937bd2a35559016e
AaronLobitana <faaron.lobitana@gmail.com>
2023-03-23 13:14:41 -0400

    badge-search
    Working on search functionality

--- src/badge-search.js
+++ src/badge-search.js
@@ -5,82 +5,76 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 class BadgeSearch extends LitElement {
   static properties = {
     header: { type: String },
+    data: { type: Array },
+    searchForThis: { type: String }
   }
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--badge-search-background-color);
+
+    h2{
+      font-size: 20px;
     }
 
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
+    .blah{
+      font-size: 20px;
+      color: red;
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = 'Badge Search';
+    this.data = 
+    [
+      {
+        "name": "john",
+        "age": "20",
+        "job": "developer"
+      },
+      {
+        "name": "tim",
+        "age": "40",
+        "job": "engineer"
+      },
+      {
+        "name": "duncan",
+        "age": "20",
+        "job": "software developer"
+      }
+    ];    
+    this.searchForThis = 'dev';
+
+    this.searchThis(this.data, this.searchForThis);
   }
+
+  searchThis(Array, searchForThis)
+    {
+      return Array.filter((thing) => 
+      {
+        for (var item in thing)
+        {
+          if (thing[item].toString().toLowerCase().includes(this.searchForThis.toLowerCase()))
+          {
+            return true;
+          }
+        }
+        return false;
+      });
+    }
+
+  
+  
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
 
-        <p>Edit <code>src/BadgeSearch.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
+      <div class="blah">
+        <h2>You are seaching for: ${this.searchForThis}</h2>
+        <h2>${this.searchThis(this.data,this.searchForThis)}</h2>
+        <h2>${this.header}</h2>
+      </div>
 
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      
     `;
   }
 }
